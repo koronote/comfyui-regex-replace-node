64030be9792200758c56c92db38316491dbdcaf2
kuro <kuroapple@gmail.com>
2026-05-08 16:54:49 +0900

    Change 行コメントの削除は、空白ではなくカンマに変換するようにした

--- nodes/regex_replace.py
+++ nodes/regex_replace.py
@@ -24,7 +24,6 @@ class RegexReplaceNode:
         for i in range(1, input_count + 1):
             t = kwargs.get(f"text_{i}") or ""
             t = unicodedata.normalize("NFKC", t)
-            # t = re.sub(r'\n', ",", t)
             if t.strip():
                 texts.append(t + "\n")
 
@@ -43,7 +42,7 @@ class RegexReplaceNode:
                 replaced = merged
 
             replaced = re.sub(r'/\*.*?\*/', "", replaced)
-            replaced = re.sub(r'//.*|#.*|\n', "", replaced)
+            replaced = re.sub(r'//.*|#.*|\n', ",", replaced)
             replaced = re.sub(r'\s{2,}', " ", replaced)
             replaced = re.sub(r',\s+', ",", replaced)
             replaced = re.sub(r'\s+,', ",", replaced)
